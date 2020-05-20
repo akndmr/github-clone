a6f2c37b0a0ae4cec3f4d494f410570aaae81a02
Sami DELİCELİ <sami.deliceli@mobillium.com>
2020-05-20 05:04:07 +0300

    project and app level build.gradle files converted to build.gradle.ktx and edited due to this editing

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+import dependencies.Dependencies
+import dependencies.TestDependencies
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion("29.0.3")
+
+    defaultConfig {
+        applicationId = BuildAndroidConfig.APPLICATION_ID
+        minSdkVersion(BuildAndroidConfig.MIN_SDK_VERSION)
+        targetSdkVersion(BuildAndroidConfig.TARGET_SDK_VERSION)
+        buildToolsVersion(BuildAndroidConfig.BUILD_TOOLS_VERSION)
+
+        versionCode = BuildAndroidConfig.VERSION_CODE
+        versionName = BuildAndroidConfig.VERSION_NAME
+
+        vectorDrawables.useSupportLibrary = BuildAndroidConfig.SUPPORT_LIBRARY_VECTOR_DRAWABLES
+        testInstrumentationRunner = BuildAndroidConfig.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+}
+
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Dependencies.KOTLIN)
+    implementation(Dependencies.APPCOMPAT)
+    implementation(Dependencies.APPCOMPAT)
+
+    testImplementation(TestDependencies.JUNIT)
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name="github-clone"
+include( "app", "buildSrc")
+
+
+
+rootProject.buildFileName = "build.gradle.kts"
