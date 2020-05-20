b07432902a8c066b6045fe6802733da3717b31e0
Sami DELİCELİ <sami.deliceli@mobillium.com>
2020-05-20 05:02:55 +0300

    buildSrc sub dependencies files added

---
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -0,0 +1,11 @@
+package dependencies
+
+import BuildDependenciesVersions
+
+object Dependencies {
+
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${BuildDependenciesVersions.KOTLIN}"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:${BuildDependenciesVersions.APPCOMPAT}"
+    const val CORE_KTX = "androidx.core:core-ktx:${BuildDependenciesVersions.CORE_KTX}"
+
+}
